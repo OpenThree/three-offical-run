3bf8fbda0b9fd8959017bbc4de1775855c13af70
zhangjingguang <2586300277@qq.com>
2025-04-01 15:38:29 +0800

    basic

--- examples/config/links.js
+++ examples/config/links.js
@@ -3,14 +3,17 @@ import { HOST } from "./host.js"
 // 企业可根据自己的需求修改url 成自己的导航地址 链接
 export default {
     site: {
-        name: 'Three JS',
+        name: 'Three JS Ⓡ175',
         url: 'https://threejs.org/',
         logo: HOST + 'files/site/logo.svg',
         footName: '- Home',
         footLink: HOST
     },
     links: [
-
+        {
+            name: '🏠Official-Basic',
+            url: 'https://openthree.github.io/three-official-examples'
+        },
         {
             name: '💎WebGL',
             url: 'https://openthree.github.io/three-cesium-examples/'
